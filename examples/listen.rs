56d1ba9f46b3ed6e517955492a2194abe21904a0
Asur4s <todaymofish@gmail.com>
2023-02-24 11:09:29 +0800

    fmt: listen

--- examples/listen.rs
+++ examples/listen.rs
@@ -6,15 +6,7 @@ fn main() -> anyhow::Result<()> {
     std::env::set_var("RUST_LOG", "trace");
 
     let mut listener = Listener::new()?;
-    dbg!("new");
     listener.run_loop()?;
 
     Ok(())
-
-    // unsafe {
-    //     let current_window_thread_id = GetWindowThreadProcessId(GetForegroundWindow(), null_mut());
-    //     let _hkl = GetKeyboardLayout(current_window_thread_id);
-    // }
-
-    // Keyboard::new().get_current_modifiers();
 }
